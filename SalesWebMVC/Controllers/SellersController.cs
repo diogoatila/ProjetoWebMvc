a3e8c0b36ee59ee9da79cdd941fffd3eecf61dd1
Diogo Carvalho <diogo.atila@headnet.com.br>
2020-09-01 16:26:49 -0300

    Tela de detalhes dos vendedores

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -66,5 +66,20 @@ namespace SalesWebMVC.Controllers
 
         }
 
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
     }
 }
